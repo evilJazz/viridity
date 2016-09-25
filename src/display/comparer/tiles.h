465ae3e13cc0831db9187e7ad3e5a2fceb2d4d06
Andre H. Beckedorf <evilJazz@katastrophos.net>
2016-09-25 18:40:11 +0000

    * Changed from proprietary license to AGPL 3.0 and commercial license;  * Added license file;  * Added license boilerplate to every Viridity file.
    git-svn-id: https://svn.katastrophos.net/viridity/trunk@259 8fbadf66-eaf5-7f48-aafb-8cadc1e9eb78

--- src/display/comparer/tiles.h
+++ src/display/comparer/tiles.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+**
+** Copyright (C) 2012-2016 Andre Beckedorf, Meteora Softworks
+** Contact: info@meteorasoftworks.com
+**
+** This file is part of Viridity
+**
+** $VIRIDITY_BEGIN_LICENSE:COMMERCIAL_AGPL$
+**
+** This library is licensed under either a separately available commercial
+** license or the GNU Affero General Public License Version 3.0,
+** published 19 November 2007.
+**
+** See https://www.gnu.org/licenses/agpl-3.0.html or LICENSE-agpl-3.0.txt for
+** details.
+**
+** If you wish to use and distribute the Viridity library in your commercial
+** product without making your sourcecode available to the public, please
+** contact us for a commercial license at info@meteorasoftworks.com
+**
+** $VIRIDITY_END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef TILES_H
 #define TILES_H
 
