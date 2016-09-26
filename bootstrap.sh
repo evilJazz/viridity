a5b02a6c1473b993be03dc2ee792db462646f82f
Andre H. Beckedorf <evilJazz@katastrophos.net>
2016-09-26 13:13:40 +0200

    Updated bootstrap.sh script to be more robust.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+SCRIPT_FILENAME="`cd \`dirname \"$0\"\`; pwd`/`basename \"$0\"`"
+SCRIPT_ROOT=$(dirname "$SCRIPT_FILENAME")
+cd "$SCRIPT_ROOT"
 
-git clone https://git.katastrophos.net/kcl.git 3rdparty/kcl
+if [ ! -d 3rdparty/kcl ]; then
+   git clone https://git.katastrophos.net/kcl.git 3rdparty/kcl
+else
+   cd 3rdparty/kcl
+   git pull
+fi
+
