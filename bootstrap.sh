b3680d3551543c77404c2a1e58d11b1c553440ba
Andre H. Beckedorf <evilJazz@katastrophos.net>
2016-11-06 14:39:29 +0100

    Revised bootstrap.sh script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,11 @@ SCRIPT_FILENAME="`cd \`dirname \"$0\"\`; pwd`/`basename \"$0\"`"
 SCRIPT_ROOT=$(dirname "$SCRIPT_FILENAME")
 cd "$SCRIPT_ROOT"
 
-if [ ! -d 3rdparty/kcl ]; then
+set -e
+
+if [ ! -f 3rdparty/kcl/kcl.pri ]; then
    git clone https://git.katastrophos.net/kcl.git 3rdparty/kcl
 else
    cd 3rdparty/kcl
    git pull
 fi
-
