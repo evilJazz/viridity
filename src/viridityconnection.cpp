e4f249f9434bdd31beecb2418dd5753e55b82c55
Andre H. Beckedorf <evilJazz@katastrophos.net>
2016-11-23 23:20:02 +0100

    Added new private class to separate connection related code from webserver code.

--- src/viridityconnection.cpp
+++ src/viridityconnection.cpp
@@ -1,6 +1,256 @@
+/****************************************************************************
+**
+** Copyright (C) 2012-2016 Andre Beckedorf, Meteora Softworks
+** Contact: info@meteorasoftworks.com
+**
+** This file is part of Viridity
+**
+** $VIRIDITY_BEGIN_LICENSE:COMMERCIAL_AGPL$
+**
+** This library is licensed under either a separately available commercial
+** license or the GNU Affero General Public License Version 3.0,
+** published 19 November 2007.
+**
+** See https://www.gnu.org/licenses/agpl-3.0.html or LICENSE-agpl-3.0.txt for
+** details.
+**
+** If you wish to use and distribute the Viridity library in your commercial
+** product without making your sourcecode available to the public, please
+** contact us for a commercial license at info@meteorasoftworks.com
+**
+** $VIRIDITY_END_LICENSE$
+**
+****************************************************************************/
+
 #include "viridityconnection.h"
 
-ViridityConnection::ViridityConnection(QObject *parent) : QObject(parent)
-{
+#include <QThread>
+#include <QtNetwork/QTcpSocket>
+#include <QtNetwork/QHostAddress>
 
+#include "viriditywebserver.h"
+
+#include "handlers/inputposthandler.h"
+#include "handlers/websockethandler.h"
+#include "handlers/ssehandler.h"
+#include "handlers/longpollinghandler.h"
+
+#ifndef VIRIDITY_DEBUG
+#undef DEBUG
+#endif
+#include "KCL/debug.h"
+
+/* ViridityTcpSocket */
+
+ViridityTcpSocket::ViridityTcpSocket(QSharedPointer<ViridityConnection> connection) :
+    QTcpSocket(),
+    connection_(connection)
+{
+    DGUARDMETHODTIMED;
 }
+
+ViridityTcpSocket::~ViridityTcpSocket()
+{
+    DGUARDMETHODTIMED;
+}
+
+void ViridityTcpSocket::sharedPointerDeleteLater(ViridityTcpSocket *socket)
+{
+    DGUARDMETHODTIMED;
+    socket->deleteLater();
+}
+
+
+/* ViridityConnection */
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+ViridityConnection::ViridityConnection(ViridityWebServer *server, qintptr socketDescriptor) :
+#else
+ViridityConnection::ViridityConnection(ViridityWebServer *server, int socketDescriptor) :
+#endif
+    QObject(),
+    sharedPointerMREW_(QReadWriteLock::Recursive),
+    webSocketHandler_(NULL),
+    sseHandler_(NULL),
+    longPollingHandler_(NULL),
+    server_(server),
+    socket_(),
+    request_(),
+    response_(),
+    socketDescriptor_(socketDescriptor),
+    reUseCount_(0)
+{
+    DGUARDMETHODTIMED;
+    created_.start();
+    lastUsed_.start();
+}
+
+ViridityConnection::~ViridityConnection()
+{
+    DGUARDMETHODTIMED;
+    close();
+    handleSocketDisconnected();
+}
+
+QVariant ViridityConnection::stats()
+{
+    QReadLocker l(&sharedPointerMREW_);
+
+    QVariantMap result;
+
+    result.insert("request.url", !request_.isNull() ? request_->url() : QVariant());
+    result.insert("request.method", !request_.isNull() ? request_->method() : QVariant());
+
+    result.insert("socket.peerAddress", !socket_.isNull() && socket_->state() == QAbstractSocket::ConnectedState ? socket_->peerAddress().toString() : QVariant());
+
+    result.insert("socketDescriptor", socketDescriptor());
+    result.insert("age", created_.elapsed());
+    result.insert("lastUsed", lastUsed_.elapsed());
+    result.insert("reUseCount", reUseCount_);
+    result.insert("livingInThread", thread()->objectName());
+
+    return result;
+}
+
+void ViridityConnection::close(QSharedPointer<ViridityConnection> reference)
+{
+    DGUARDMETHODTIMED;
+
+    if (socket_ && socket_->isOpen())
+        socket_->close();
+}
+
+void ViridityConnection::sharedPointerDeleteLater(ViridityConnection *connection)
+{
+    connection->deleteLater();
+}
+
+void ViridityConnection::setupConnection(QSharedPointer<ViridityConnection> reference)
+{
+    DGUARDMETHODTIMED;
+    if (reference.data() != this) return;
+
+    QWriteLocker l(&sharedPointerMREW_);
+
+    // Open socket
+    socket_ = QSharedPointer<ViridityTcpSocket>(
+        new ViridityTcpSocket(reference),
+        &ViridityTcpSocket::sharedPointerDeleteLater
+    );
+
+    connect(socket_.data(), SIGNAL(disconnected()), this, SLOT(handleSocketDisconnected()));
+
+    // Attach incomming connection to socket
+    if (!socket_->setSocketDescriptor(socketDescriptor_))
+    {
+        socket_.clear();
+        return;
+    }
+
+    // Hand-off incoming connection to Tufao to parse request...
+    request_ = QSharedPointer<ViridityHttpServerRequest>(
+        new ViridityHttpServerRequest(socket_),
+        &ViridityHttpServerRequest::sharedPointerDeleteLater
+    );
+
+    connect(request_.data(), SIGNAL(ready()), this, SLOT(handleRequestReady()));
+    connect(request_.data(), SIGNAL(upgrade(QByteArray)), this, SLOT(handleRequestUpgrade(QByteArray)));
+
+    DPRINTF("New connection from %s.", socket_->peerAddress().toString().toLatin1().constData());
+}
+
+void ViridityConnection::handleSocketDisconnected()
+{
+    DGUARDMETHODTIMED;
+
+    // This mutex is required because we need exclusive access when changing our shared pointer members that are accessed from different threads!
+    QWriteLocker l(&sharedPointerMREW_);
+
+    handleResponseFinished();
+
+    if (request_)
+    {
+        disconnect(request_.data(), SIGNAL(ready()), this, SLOT(handleRequestReady()));
+        disconnect(request_.data(), SIGNAL(upgrade(QByteArray)), this, SLOT(handleRequestUpgrade(QByteArray)));
+    }
+
+    request_.clear();
+
+    // Deref the socket. If no other requests or responses hold references, it will be freed.
+    // If it will be freed and it holds the last reference to this connection, the connection will also be freed.
+    if (socket_)
+        disconnect(socket_.data(), SIGNAL(disconnected()), this, SLOT(handleSocketDisconnected()));
+
+    socket_.clear();
+}
+
+void ViridityConnection::handleResponseFinished()
+{
+    DGUARDMETHODTIMED;
+
+    // This mutex is required because we need exclusive access when changing our shared pointer members that are accessed from different threads!
+    QWriteLocker l(&sharedPointerMREW_);
+
+    if (response_)
+        disconnect(response_.data(), SIGNAL(finished()), this, SLOT(handleResponseFinished()));
+
+    response_.clear();
+}
+
+void ViridityConnection::handleRequestReady()
+{
+    //DGUARDMETHODTIMED;
+
+    lastUsed_.restart();
+    ++reUseCount_;
+
+    DPRINTF("Request for %s of connection from %s ready.", request_->url().constData(), request_->getPeerAddressFromRequest().constData());
+
+    response_ = QSharedPointer<ViridityHttpServerResponse>(
+        new ViridityHttpServerResponse(socket_, request_->responseOptions()),
+        &ViridityHttpServerResponse::sharedPointerDeleteLater
+    );
+
+    connect(response_.data(), SIGNAL(finished()), this, SLOT(handleResponseFinished()));
+
+    if (request_->headers().contains("Expect", "100-continue"))
+        response_->writeContinue();
+
+    if (server_->doesHandleRequest(request_))
+    {
+        server_->handleRequest(request_, response_);
+    }
+    else if (SSEHandler::staticDoesHandleRequest(server_, request_))
+    {
+        if (!sseHandler_)
+            sseHandler_ = new SSEHandler(server_, this);
+
+        sseHandler_->handleRequest(request_, response_);
+    }
+    else if (LongPollingHandler::staticDoesHandleRequest(server_, request_))
+    {
+        if (!longPollingHandler_)
+            longPollingHandler_ = new LongPollingHandler(server_, this);
+
+        longPollingHandler_->handleRequest(request_, response_);
+    }
+    else
+    {
+        response_->writeHead(404);
+        response_->end("Not found");
+    }
+}
+
+void ViridityConnection::handleRequestUpgrade(const QByteArray &head)
+{
+    lastUsed_.restart();
+    ++reUseCount_;
+
+    DPRINTF("Request for %s of connection from %s wants upgrade.", request_->url().constData(), request_->getPeerAddressFromRequest().constData());
+
+    if (!webSocketHandler_)
+        webSocketHandler_ = new WebSocketHandler(server_, this);
+
+    webSocketHandler_->handleUpgrade(request_, head);
+}
+
--- src/viridityconnection.h
+++ src/viridityconnection.h
@@ -1,17 +1,124 @@
+/****************************************************************************
+**
+** Copyright (C) 2012-2016 Andre Beckedorf, Meteora Softworks
+** Contact: info@meteorasoftworks.com
+**
+** This file is part of Viridity
+**
+** $VIRIDITY_BEGIN_LICENSE:COMMERCIAL_AGPL$
+**
+** This library is licensed under either a separately available commercial
+** license or the GNU Affero General Public License Version 3.0,
+** published 19 November 2007.
+**
+** See https://www.gnu.org/licenses/agpl-3.0.html or LICENSE-agpl-3.0.txt for
+** details.
+**
+** If you wish to use and distribute the Viridity library in your commercial
+** product without making your sourcecode available to the public, please
+** contact us for a commercial license at info@meteorasoftworks.com
+**
+** $VIRIDITY_END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef VIRIDITYCONNECTION_H
 #define VIRIDITYCONNECTION_H
 
 #include <QObject>
+#include <QTime>
+#include <QTcpSocket>
+#include <QReadWriteLock>
+#include <QSharedPointer>
+
+class WebSocketHandler;
+class SSEHandler;
+class LongPollingHandler;
+
+class ViridityHttpServerRequest;
+class ViridityHttpServerResponse;
+class ViridityWebServer;
+class ViridityConnection;
+
+
+/* ViridityTcpSocket */
+
+class ViridityTcpSocket : public QTcpSocket
+{
+    Q_OBJECT
+public:
+    explicit ViridityTcpSocket(QSharedPointer<ViridityConnection> connection);
+    virtual ~ViridityTcpSocket();
+
+protected:
+    friend class ViridityConnection;
+    static void sharedPointerDeleteLater(ViridityTcpSocket *socket);
+
+private:
+    QSharedPointer<ViridityConnection> connection_;
+};
+
+
+/* ViridityConnection */
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    typedef qintptr ViriditySocketDescriptor;
+#else
+    typedef int ViriditySocketDescriptor;
+#endif
 
 class ViridityConnection : public QObject
 {
     Q_OBJECT
 public:
-    explicit ViridityConnection(QObject *parent = 0);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    explicit ViridityConnection(ViridityWebServer *server, qintptr socketDescriptor);
+#else
+    explicit ViridityConnection(ViridityWebServer *server, int socketDescriptor);
+#endif
 
-signals:
+    virtual ~ViridityConnection();
+
+    ViriditySocketDescriptor socketDescriptor() const { return socketDescriptor_; }
+
+    ViridityWebServer *server() { return server_; }
+
+    QVariant stats();
 
 public slots:
+    void setupConnection(QSharedPointer<ViridityConnection> reference);
+    void close(QSharedPointer<ViridityConnection> reference = QSharedPointer<ViridityConnection>());
+
+protected:
+    friend class ViridityWebServer;
+    static void sharedPointerDeleteLater(ViridityConnection *connection);
+
+private slots:
+    void handleRequestReady();
+    void handleRequestUpgrade(const QByteArray &);
+    void handleResponseFinished();
+    void handleSocketDisconnected();
+
+private:
+    mutable QReadWriteLock sharedPointerMREW_;
+
+    WebSocketHandler *webSocketHandler_;
+    SSEHandler *sseHandler_;
+    LongPollingHandler *longPollingHandler_;
+
+    ViridityWebServer *server_;
+
+    QSharedPointer<ViridityTcpSocket> socket_;
+    QSharedPointer<ViridityHttpServerRequest> request_;
+    QSharedPointer<ViridityHttpServerResponse> response_;
+
+    ViriditySocketDescriptor socketDescriptor_;
+
+    QTime created_;
+    QTime lastUsed_;
+    int reUseCount_;
 };
+
+Q_DECLARE_METATYPE( QSharedPointer<ViridityConnection> )
 
 #endif // VIRIDITYCONNECTION_H
