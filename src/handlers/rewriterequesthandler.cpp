0033a8436942fe37c17662df283bd26210f11aa4
Andre H. Beckedorf <evilJazz@katastrophos.net>
2017-10-12 02:53:14 +0200

    Implemented support for external rewrites/location redirects in RewriteRequestHandler.

--- src/handlers/rewriterequesthandler.cpp
+++ src/handlers/rewriterequesthandler.cpp
@@ -1,6 +1,82 @@
+/****************************************************************************
+**
+** Copyright (C) 2012-2017 Andre Beckedorf, Meteora Softworks
+** Contact: info@meteorasoftworks.com
+**
+** This file is part of Viridity
+**
+** $VIRIDITY_BEGIN_LICENSE:COMMERCIAL_AGPL$
+**
+** This library is licensed under either a separately available commercial
+** license or the GNU Affero General Public License Version 3.0,
+** published 19 November 2007.
+**
+** See https://www.gnu.org/licenses/agpl-3.0.html or LICENSE-agpl-3.0.txt for
+** details.
+**
+** If you wish to use and distribute the Viridity library in your commercial
+** product without making your sourcecode available to the public, please
+** contact us for a commercial license at info@meteorasoftworks.com
+**
+** $VIRIDITY_END_LICENSE$
+**
+****************************************************************************/
+
 #include "rewriterequesthandler.h"
 
 #include "KCL/debug.h"
+
+bool RewriteRequestHandler::RewriteRule::interpretRule(const QString &rule)
+{
+    QStringList ruleParts = rule.trimmed().split(" ");
+
+    if (ruleParts.count() > 1)
+    {
+        this->pattern = ruleParts.at(0);
+        this->substitution = ruleParts.at(1);
+
+        if (ruleParts.count() > 2)
+        {
+            if (ruleParts.at(2).startsWith("[R"))
+            {
+                this->flag = RewriteExternal;
+
+                QRegExp re("=([0-9]{3})\\]");
+                if (re.indexIn(ruleParts.at(2)) > -1)
+                    this->rewriteHttpStatusCode = re.capturedTexts().at(1).toInt();
+                else
+                    this->rewriteHttpStatusCode = 301;
+            }
+        }
+        else
+            this->flag = RewriteInternal;
+
+        return true;
+    }
+
+    return false;
+}
+
+QString RewriteRequestHandler::RewriteRule::doesMatchAndSubstitute(const QString &input) const
+{
+    QRegExp re(this->pattern);
+
+    if (re.indexIn(input) > -1)
+    {
+        QString ruleRewrite = this->substitution;
+
+        QStringList capturedTexts = re.capturedTexts();
+        for (int i = 0; i < capturedTexts.length(); ++i)
+        {
+            QString cap = capturedTexts.at(i);
+            ruleRewrite.replace("$" + QString::number(i), cap);
+        }
+
+        return ruleRewrite;
+    }
+
+    return QString::null;
+}
 
 RewriteRequestHandler::RewriteRequestHandler(ViridityWebServer *server, QObject *parent) :
     ViridityBaseRequestHandler(server, parent)
@@ -11,14 +87,31 @@ RewriteRequestHandler::~RewriteRequestHandler()
 {
 }
 
-void RewriteRequestHandler::addRule(const QString &rule)
+bool RewriteRequestHandler::addRule(const QString &rule)
 {
-    rewriteRules_.append(rule);
+    RewriteRule irule;
+    if (irule.interpretRule(rule))
+    {
+        interpretedRewriteRules_.append(irule);
+        rewriteRules_.append(rule);
+        return true;
+    }
+
+    return false;
 }
 
-void RewriteRequestHandler::removeRule(const QString &rule)
+bool RewriteRequestHandler::removeRule(const QString &rule)
 {
-    rewriteRules_.removeOne(rule);
+    int index = rewriteRules_.indexOf(rule);
+
+    if (index > -1)
+    {
+        rewriteRules_.removeAt(index);
+        interpretedRewriteRules_.removeAt(index);
+        return true;
+    }
+
+    return false;
 }
 
 void RewriteRequestHandler::clear()
@@ -30,29 +123,25 @@ bool RewriteRequestHandler::doesHandleRequest(QSharedPointer<ViridityHttpServerR
 {
     QString url = request->url();
 
-    foreach (const QString &rule, rewriteRules_)
+    foreach (const RewriteRule &rule, interpretedRewriteRules_)
     {
-        QStringList ruleParts = rule.trimmed().split(" ");
-
-        if (ruleParts.count() > 1)
+        if (rule.flag == RewriteInternal)
         {
-            QRegExp re(ruleParts.at(0));
+            QString substitutedUrl = rule.doesMatchAndSubstitute(url);
 
-            if (re.indexIn(url) > -1)
+            if (!substitutedUrl.isNull())
             {
-                QString ruleRewrite = ruleParts.at(1);
+                DPRINTF("Internally rewriting url %s to %s", request->url().constData(), substitutedUrl.toLatin1().constData());
 
-                QStringList capturedTexts = re.capturedTexts();
-                for (int i = 0; i < capturedTexts.length(); ++i)
-                {
-                    QString cap = capturedTexts.at(i);
-                    ruleRewrite.replace("$" + QString::number(i), cap);
-                }
-
-                DPRINTF("Rewriting url %s to %s", request->url().constData(), ruleRewrite.toLatin1().constData());
-
-                request->rewriteUrl(ruleRewrite.toLatin1());
+                request->rewriteUrl(substitutedUrl.toLatin1());
             }
+        }
+        else if (rule.flag == RewriteExternal)
+        {
+            QString substitutedUrl = rule.doesMatchAndSubstitute(url);
+
+            if (!substitutedUrl.isNull())
+                return true;
         }
     }
 
@@ -61,5 +150,23 @@ bool RewriteRequestHandler::doesHandleRequest(QSharedPointer<ViridityHttpServerR
 
 void RewriteRequestHandler::handleRequest(QSharedPointer<ViridityHttpServerRequest> request, QSharedPointer<ViridityHttpServerResponse> response)
 {
-    // Does not handle any request itself...
+    QString url = request->url();
+
+    foreach (const RewriteRule &rule, interpretedRewriteRules_)
+    {
+        if (rule.flag == RewriteExternal)
+        {
+            QString substitutedUrl = rule.doesMatchAndSubstitute(url);
+
+            if (!substitutedUrl.isNull())
+            {
+                DPRINTF("Externally rewriting url %s to %s", request->url().constData(), substitutedUrl.toLatin1().constData());
+
+                response->headers().insert("Location", substitutedUrl.toLatin1());
+                response->writeHead(rule.rewriteHttpStatusCode);
+                response->end();
+            }
+        }
+    }
 }
+
