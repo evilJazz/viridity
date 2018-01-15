7761be04a5f4c40654a027c43a0d8e009e43208e
Andre H. Beckedorf <evilJazz@katastrophos.net>
2018-01-15 23:07:53 +0100

    Fixed up QML preprocessor helper scripts.

--- qmlppQtQuick2.sh
+++ qmlppQtQuick2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-3rdparty/kcl/qmlpp/src/qmlpp.sh -i -d "@QtQuick2" -q 2.2 tests/testdata/qml/ examples
+3rdparty/kcl/qmlpp/src/qmlpp.sh -i -d "@QtQuick2" -q 2.2 src/qml/ tests/testdata/qml/ examples
 echo "QML and JS files rewritten for QtQuick 2."
