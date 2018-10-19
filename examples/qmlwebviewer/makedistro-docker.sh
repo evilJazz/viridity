41074855aa17988dee66dd8e41a75f5d9e5eb5f7
Andre H. Beckedorf <evilJazz@katastrophos.net>
2018-10-19 08:37:26 +0200

    Updated scripts to make bundling of Qt libs optional.

--- examples/qmlwebviewer/makedistro-docker.sh
+++ examples/qmlwebviewer/makedistro-docker.sh
@@ -4,12 +4,21 @@ DISTRO_ROOT=$(dirname "$SCRIPT_FILENAME")
 
 function usage()
 {
-    echo $0 [TARGETROOT] [TARGETNAME] {filename of docker image}
+    echo $0 [--bundle-libs] [TARGETROOT] [TARGETNAME] {filename of docker image}
     echo
+    echo "  --bundle-libs    Bundle Qt libraries with tarball"
     echo "  TARGETROOT = the install path defined in the .pro/Makefile, e.g. /opt/qmlwebviewer"
     echo "  TARGETNAME = the name of the binary, e.g. qmlwebviewer"
     echo
+    echo "NOTE: Required the QTDIR environment variable to be defined."
+    echo
 }
+
+BUNDLE_LIBS_PARAM=
+if [ "$1" == "--bundle-libs" ]; then
+    BUNDLE_LIBS_PARAM=--bundle-libs
+    shift 1
+fi
 
 TARGETROOT="$1"
 TARGETNAME="$2"
@@ -26,13 +35,15 @@ rm -Rf docker
 mkdir docker
 cd docker
 
-OUTPUTTARBALL="$DISTRO_ROOT/docker/output.tar.gz"
+mkdir files
 
-$DISTRO_ROOT/makedistro-tarball.sh "$TARGETROOT" "$OUTPUTTARBALL"
+OUTPUTTARBALL="$DISTRO_ROOT/docker/files/output.tar.gz"
 
-echo "Building Docker image..."
+$DISTRO_ROOT/makedistro-tarball.sh $BUNDLE_LIBS_PARAM "$TARGETROOT" "$OUTPUTTARBALL"
 
-cat > run.sh << RUNSH
+echo "Assembling Docker image..."
+
+cat > files/run.sh << RUNSH
 #!/bin/sh
 cd "$TARGETROOT"/bin
 
@@ -42,7 +53,7 @@ export LANGUAGE="de_DE"
 QT_PLUGIN_PATH="$TARGETROOT/plugins" xvfb-run -a -s "-screen 0 640x480x24" "./$TARGETNAME" \$@
 RUNSH
 
-chmod +x run.sh
+chmod +x files/run.sh
 
 cat > Dockerfile << DOCKER
 FROM ubuntu:18.04
@@ -50,7 +61,15 @@ ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && apt-get -y install \
     fontconfig libjpeg8 xvfb libxrender1 libxcomposite1 libegl1-mesa libxslt1.1 libxi6 libfontconfig1 libglib2.0-0 \
     libdbus-1-3 libicu60 libasound2 libpulse-mainloop-glib0 libpulse0 libssl1.0.0 libssl1.0-dev ca-certificates
+DOCKER
 
+if [ -z "$BUNDLE_LIBS_PARAM" ]; then
+    cat >> Dockerfile << DOCKER
+RUN apt-get -y install qt5-default qml libqt5qml5 libqt5quick5 libjpeg
+DOCKER
+fi
+
+cat >> Dockerfile << DOCKER
 RUN mkdir /data
 RUN mkdir /config
 RUN chown www-data.www-data config
@@ -60,16 +79,18 @@ VOLUME /config
 
 EXPOSE 8080
 
-ADD "output.tar.gz" /
-ADD "run.sh" "$TARGETROOT"
+ADD "files/output.tar.gz" /
+ADD "files/run.sh" "$TARGETROOT"
 
 USER www-data
 ENTRYPOINT ["$TARGETROOT/run.sh"]
 DOCKER
 
-docker build -t "$TARGETNAME" .
-
 if [ ! -z "$OUTPUTNAME" ]; then
+    echo "Building Docker image..."
+    docker build -t "$TARGETNAME" .
     echo "Exporting Docker image..."
     docker save "$TARGETNAME" | gzip -9 > "$OUTPUTNAME"
 fi
+
+echo "Done."
--- examples/qmlwebviewer/makedistro-tarball.sh
+++ examples/qmlwebviewer/makedistro-tarball.sh
@@ -4,11 +4,20 @@ DISTRO_ROOT=$(dirname "$SCRIPT_FILENAME")
 
 function usage()
 {
-    echo $0 [TARGETROOT] [filename of output tar.gz]
+    echo "Usage: $0 [--bundle-libs] [TARGETROOT] [filename of output tar.gz]"
     echo
+    echo "  --bundle-libs    Bundle Qt libraries with tarball"
     echo "  TARGETROOT, e.g. /opt/qmlwebviewer"
     echo
+    echo "NOTE: Required the QTDIR environment variable to be defined."
+    echo
 }
+
+BUNDLE_LIBS=0
+if [ "$1" == "--bundle-libs" ]; then
+    BUNDLE_LIBS=1
+    shift 1
+fi
 
 TARGETROOT="$1"
 OUTPUTNAME="$2"
@@ -43,53 +52,52 @@ OPT_ROOT="$PWD/dist/$TARGETROOT"
 
 function copyLibDir()
 {
-        [ -d "$2" ] || mkdir -p "$2" || return 1
-        rsync -rltuv --exclude=*plugind.dll --exclude=*d.dll --exclude=*.pdb $@
+    [ -d "$2" ] || mkdir -p "$2" || return 1
+    rsync -rltuv --exclude=*plugind.dll --exclude=*d.dll --exclude=*.pdb $@
 }
 
 function copyQtFiles()
 {
-        cat > "$OPT_ROOT/bin/qt.conf" << EOF
+    cat > "$OPT_ROOT/bin/qt.conf" << EOF
 [Paths]
 Prefix = ..
 EOF
 
-        # Almost the same dependencies as on Android...
-        copyLibDir "$QTDIR/lib/libicu*" "$OPT_ROOT/lib/" || return
-        copyLibDir "$QTDIR/lib/libQt5Core.so*" "$OPT_ROOT/lib/" || return
-        copyLibDir "$QTDIR/lib/libQt5DBus.so*" "$OPT_ROOT/lib/" || return 11
-        copyLibDir "$QTDIR/lib/libQt5Gui.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Widgets.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Qml.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Quick.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Network.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Concurrent.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Sql.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Svg.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5Multimedia.so*" "$OPT_ROOT/lib/" || return 1
-        copyLibDir "$QTDIR/lib/libQt5XcbQpa.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libicu*" "$OPT_ROOT/lib/" || return
+    copyLibDir "$QTDIR/lib/libQt5Core.so*" "$OPT_ROOT/lib/" || return
+    copyLibDir "$QTDIR/lib/libQt5DBus.so*" "$OPT_ROOT/lib/" || return 11
+    copyLibDir "$QTDIR/lib/libQt5Gui.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Widgets.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Qml.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Quick.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Network.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Concurrent.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Sql.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Svg.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5Multimedia.so*" "$OPT_ROOT/lib/" || return 1
+    copyLibDir "$QTDIR/lib/libQt5XcbQpa.so*" "$OPT_ROOT/lib/" || return 1
 
-        copyLibDir "$QTDIR/qml/Qt" "$OPT_ROOT/qml/" || return 1
-        copyLibDir "$QTDIR/qml/QtQml" "$OPT_ROOT/qml/" || return 1
-        copyLibDir "$QTDIR/qml/QtQuick" "$OPT_ROOT/qml/" || return 1
-        copyLibDir "$QTDIR/qml/QtQuick.2" "$OPT_ROOT/qml/" || return 1
+    copyLibDir "$QTDIR/qml/Qt" "$OPT_ROOT/qml/" || return 1
+    copyLibDir "$QTDIR/qml/QtQml" "$OPT_ROOT/qml/" || return 1
+    copyLibDir "$QTDIR/qml/QtQuick" "$OPT_ROOT/qml/" || return 1
+    copyLibDir "$QTDIR/qml/QtQuick.2" "$OPT_ROOT/qml/" || return 1
 
-        copyLibDir "$QTDIR/plugins/audio" "$OPT_ROOT/plugins/" || return 1
-        copyLibDir "$QTDIR/plugins/generic" "$OPT_ROOT/plugins/" || return 1
-        copyLibDir "$QTDIR/plugins/imageformats" "$OPT_ROOT/plugins/" || return 1
-        copyLibDir "$QTDIR/plugins/sqldrivers" "$OPT_ROOT/plugins/" || return 1
-        copyLibDir "$QTDIR/plugins/xcbglintegrations" "$OPT_ROOT/plugins/" || return 1
+    copyLibDir "$QTDIR/plugins/audio" "$OPT_ROOT/plugins/" || return 1
+    copyLibDir "$QTDIR/plugins/generic" "$OPT_ROOT/plugins/" || return 1
+    copyLibDir "$QTDIR/plugins/imageformats" "$OPT_ROOT/plugins/" || return 1
+    copyLibDir "$QTDIR/plugins/sqldrivers" "$OPT_ROOT/plugins/" || return 1
+    copyLibDir "$QTDIR/plugins/xcbglintegrations" "$OPT_ROOT/plugins/" || return 1
 
-        [ -d "$OPT_ROOT/plugins/platforms" ] || mkdir "$OPT_ROOT/plugins/platforms/" || return 1
-        cp -a "$QTDIR/plugins/platforms/libqxcb.so" "$OPT_ROOT/plugins/platforms/" || return 1
+    [ -d "$OPT_ROOT/plugins/platforms" ] || mkdir "$OPT_ROOT/plugins/platforms/" || return 1
+    cp -a "$QTDIR/plugins/platforms/libqxcb.so" "$OPT_ROOT/plugins/platforms/" || return 1
 
-        [ -d "$OPT_ROOT/plugins/bearer" ] || mkdir "$OPT_ROOT/plugins/bearer/" || return 1
-        cp -a "$QTDIR/plugins/bearer/libqgenericbearer.so" "$OPT_ROOT/plugins/bearer/" || return 1
+    [ -d "$OPT_ROOT/plugins/bearer" ] || mkdir "$OPT_ROOT/plugins/bearer/" || return 1
+    cp -a "$QTDIR/plugins/bearer/libqgenericbearer.so" "$OPT_ROOT/plugins/bearer/" || return 1
 
-        return 0
+    return 0
 }
 
-copyQtFiles
+[ $BUNDLE_LIBS -eq 1 ] && copyQtFiles
 
 cd dist
 tar czvf "$OUTPUTNAME" *
